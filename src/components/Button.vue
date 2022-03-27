ebcdc1abb35b806d7ae1edc18db09b2656ec8bef
pkkulhari <praveen.kulhari07@gmail.com>
2022-03-27 22:02:06 +0530

    Initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="btn" :style="{ background: color}">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonComp',
+  props: {
+    color: String,
+    text: String
+  }
+}
+</script>
---
+++ src/components/Task.vue
@@ -0,0 +1,44 @@
+<template>
+  <div @dblclick="$emit('toggle-reminder')" :class="[task.reminder ? 'reminder' : '', 'task']">
+    <h3>
+      {{ task.text }}
+      <i @click="onClick" class="fa fa-times"></i>
+    </h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskComp',
+  props: {
+    task: Object,
+  },
+  methods: {
+    onClick() {
+      this.$emit('delete-task')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.fa {
+  color: red;
+}
+
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
